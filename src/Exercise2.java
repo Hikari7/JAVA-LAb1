2ea92de355cf9d2b7dc34ed7e0f0b666ea63ec0c
Hikari7 <h.kobe712@gmail.com>
2022-06-08 21:53:12 -0700

    first commit

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 
-public class  Excercise2 {
+public class  Exercise2 {
   public static void main(String[] args) {
     
     Scanner scanner = new Scanner(System.in);
--- src/Exercise3.java
+++ src/Exercise3.java
@@ -1,4 +1,4 @@
-public class Excercise3 {
+public class Exercise3 {
 
   public static void  main(String[] args) throws Exception{
    
